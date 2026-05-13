f3e535a888c441004e93e5c5980790deb1d25ff6
Kanakasrikssn <kssnkanakasri@gmail.com>
2026-05-13 15:44:08 +0530

    Add files via upload
    On the Day 1 of my python full stack journey at Codegnan, I learnt about the basic introduction of Python , tokens , identifiers , variables and types of comments.

---
+++ Day 1.py
@@ -0,0 +1,50 @@
+print("This is my First Python class")
+"""
+ 1.what is python
+-high level language
+ no need to worry about low level managment(like memory allocation)
+------------------------------
+-dynamically typed language
+no need to mention any datatype like done in other languages
+ie in c we need to write like int a=23;
+however in python a=23 the python automatically considers it as integer
+-----------------------------
+-intrepreter language
+the program will be executed till an error is found ie the code is executed line by line,
+if an error is found in any line all the before lines will be executed however all the other lines after the error will not be executed.
+-----------------------------
+
+
+ 2.tokens(keywords used in a program)
+  if, while , for ,else, and ,or etc
+----------------------------
+
+
+  3. identifiers
+ names of variables , functions and classes
+-------------------------------
+
+ 4.variables
+ which are used to store data
+ rules for defining a variables:
+ 1.good ways( a variable can be defined with in thge start of a variable)
+   letters A-Z,a-z, _
+ 2.bad ways (except numbers though can be used in the variable they should not be in the first position)
+'   special characters, space ,0-9(digits in first position)
+'______________
+
+  5.comments
+  1. single line comments(#)
+  2.multi-line comments(''')or ("""
+'------------------'
+'''3 types of variables'''
+
+number1 = 234
+n um =23
+ print('number1')
+ print('n um')
+ 
+
+
+ 
+
